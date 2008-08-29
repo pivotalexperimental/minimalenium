5c01aeb3cde129009817efea1eefaa484a3f8a18
Chad Woolley <thewoolleyman@gmail.com>
2008-08-29 00:59:10 -0700

    trying to make rake and command line invocation both work

--- test/selenium/post_two_test.rb
+++ test/selenium/post_two_test.rb
@@ -1,17 +1,17 @@
 require File.dirname(__FILE__) + '/../test_helper'
 require 'selenium-rails'
 
-class PostTest < Selenium::TestCase
+class PostTwoTest < Selenium::TestCase
   fixtures :all
   
-  def test_post_1_1
+  def test_post_2_1
     browser.open "/posts"
     assert_equal "Posts: index", browser.get_title
     assert browser.is_text_present(posts(:one).title)
     assert browser.is_text_present(posts(:two).body)
   end
 
-  def test_post_1_2
+  def test_post_2_2
     browser.open "/posts"
     assert_equal "Posts: index", browser.get_title
     assert browser.is_text_present(posts(:one).title)
